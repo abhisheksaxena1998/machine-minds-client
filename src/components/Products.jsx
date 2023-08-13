5f349fe70a2b9243b94ee23222b1e1d87e3562c1
Abhishek <abhi123923@gmail.com>
2023-08-13 21:48:17 +0530

    added test products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,72 +3,68 @@ const { Meta } = Card;
 
 const items = [
   {
-    key: 0,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
+    category: "Top Offers",
+    items: [
+      {
+        key: 0,
+        url: "https://s.hdnux.com/photos/01/15/52/15/20357974/7/1200x0.jpg",
+        title: "Laptop",
+      },
+      {
+        key: 1,
+        url: "https://th.bing.com/th/id/OIP.2JkSNigfTvmaMwVkzc7A9wHaHa?pid=ImgDet&rs=1",
+        title: "Phone",
+      },
+      {
+        key: 2,
+        url: "https://png.pngtree.com/png-clipart/20220523/ourmid/pngtree-cool-black-realistic-eyeglasses-png-image_4707488.png",
+        title: "Sunglasses",
+      },
+      {
+        key: 3,
+        url: "https://images.unsplash.com/photo-1523293182086-7651a899d37f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyZnVtZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+        title: "Perfume",
+      },
+      {
+        key: 4,
+        url: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F61%2F70%2F61708115ff10932bd9bb771f3110089435ac345e.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main]",
+        title: "Shirts",
+      },
+    ],
   },
   {
-    key: 1,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 2,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 3,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 4,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 5,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 6,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 7,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 8,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 9,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 10,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 11,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 12,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 13,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 14,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
-  },
-  {
-    key: 15,
-    url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
+    category: "Best deals",
+    items: [
+      {
+        key: 0,
+        url: "https://m.media-amazon.com/images/I/71VjM5LOeYL._AC_UY1000_.jpg",
+        title: "Watch",
+      },
+      {
+        key: 1,
+        url: "https://rukminim2.flixcart.com/image/832/832/l51d30w0/shoe/z/w/c/10-mrj1914-10-aadi-white-black-red-original-imagft9k9hydnfjp.jpeg?q=70",
+        title: "Shoes",
+      },
+      {
+        key: 2,
+        url: "https://m.media-amazon.com/images/I/51wQ18eozvL._SX679_.jpg",
+        title: "Laptop",
+      },
+      {
+        key: 3,
+        url: "https://m.media-amazon.com/images/I/81ecSfN2OOL._AC_UY1100_.jpg",
+        title: "Wallet",
+      },
+      {
+        key: 4,
+        url: "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F2c%2F91%2F2c916835024d8f6cf56292cdc738a7da870fd4cf.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bladies_accessories_belts%5D%2Ctype%5BDESCRIPTIVEDETAIL%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
+        title: "Belt",
+      },
+    ],
   },
 ];
 
-const ProductCard = ({ url }) => (
+const ProductCard = ({ url, title }) => (
   <Card
     className="mt-4"
     hoverable
@@ -77,22 +73,27 @@ const ProductCard = ({ url }) => (
     }}
     cover={<img alt="example" src={url} />}
   >
-    <Meta
-      className="text-left"
-      title="Europe Street beat"
-      description="www.instagram.com"
-    />
-    <div className="price-tag">₹ 999</div>
+    <Meta className="text-left" title={title} description="www.instagram.com" />
+    <div className="price-tag">₹ 999XX</div>
   </Card>
 );
 
 const Products = () => (
-  <div className="mt-12">
-    <div className="container">
-      {items.map((item) => (
-        <ProductCard key={item.key} url={item.url} />
-      ))}
-    </div>
+  <div>
+    {items.map((item) => {
+      return (
+        <div key={item.category}>
+          <span className="ml-8 float-left font-bold">{item.category}</span>
+          <div className="mt-12">
+            <div className="container">
+              {item.items.map((item) => (
+                <ProductCard key={item.key} url={item.url} title={item.title} />
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    })}
   </div>
 );
 
